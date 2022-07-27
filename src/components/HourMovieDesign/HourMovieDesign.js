37cbd24b80dea2cf23ce7c4f35b5214ee47f264b
elias <eliasmanica@poli.ufrj.br>
2022-07-27 10:53:08 -0300

    exibindo horarios filmes

--- src/components/HourMovieDesign/HourMovieDesign.js
+++ src/components/HourMovieDesign/HourMovieDesign.js
@@ -1,12 +1,17 @@
 import "./style.css";
 
-export default function HourMovieDesign() {
+export default function HourMovieDesign({ weekday, date, hour }) {
   return (
-    <div className="showHour">
-      <h3>Quinta-feira - 24/06/2021</h3>
+    <div className="showHour" onClick={() => console.log(hour, weekday)}>
+      <h3>
+        {weekday} - {date}
+      </h3>
       <div className="selectHour">
-        <div className="squareHour">15:00</div>
-        <div className="squareHour">19:00</div>
+        {hour.map((value) => (
+          <div className="squareHour" key={value.id}>
+            {value.name}
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/SelectHour/SelectHour.js
+++ src/components/SelectHour/SelectHour.js
@@ -1,14 +1,36 @@
 import "./style.css";
+import React, { useEffect } from "react";
+import axios from "axios";
 import HourMovieDesign from "../HourMovieDesign/HourMovieDesign";
 
 export default function SelectHour() {
+  const [hour, setHour] = React.useState([]);
+
+  useEffect(() => {
+    const promise = axios.get(
+      "https://mock-api.driven.com.br/api/v5/cineflex/movies/1/showtimes"
+    );
+
+    promise.then((response) => {
+      setHour(response.data.days);
+    });
+  }, []);
+
+  console.log(hour);
+
   return (
     <>
       <div className="hour">
         <h2>horario</h2>
       </div>
-      <HourMovieDesign />
-      <HourMovieDesign />
+      {hour.map((movieHour) => (
+        <HourMovieDesign
+          key={movieHour.id}
+          weekday={movieHour.weekday}
+          date={movieHour.date}
+          hour={movieHour.showtimes}
+        />
+      ))}
     </>
   );
 }
